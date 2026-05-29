c70b1fea1193ee0b808b2f0e1ebab358d6622a9e
GAETANNGALEU23 <francoisngaleu232@gmail.com>
2026-05-29 20:07:51 +0200

    Fix typo in import statement for Streamlit

--- app_gem.py
+++ app_gem.py
@@ -15,7 +15,7 @@
 ========================================================================
 """
 
-import streamli as st
+import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
